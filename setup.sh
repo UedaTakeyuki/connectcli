0abac9512f84e59eeb67f353dcb9c6e0aeb76976
Takeyuki UEDA <grenouill.vert@gmail.com>
2022-08-26 21:12:47 +0900

    add service file creation from .org

--- setup.sh
+++ setup.sh
@@ -1,2 +1,25 @@
+#!/bin/bash
+
+################################################################
+# 
+# Connect client setup script
+# 
+# usage: ./autostart.sh --on/--off
+#
+#
+# @author Dr. Takeyuki UEDA
+# @copyright Copyright© Atelier UEDA 2022 - All rights reserved.
+#
+
+# 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+CMD=connectcli
+
+# download connectcli
 curl https://assets.uedasoft.com/connectcli/connectcli@ARMlatest -o connectcli -l
 chmod +x connectcli
+
+# create connectcli.service file
+sed -e "s@^WorkingDirectory=.*@WorkingDirectory=${SCRIPT_DIR}@" \
+    -e "s@^ExecStart=.*@ExecStart=${SCRIPT_DIR}/${CMD}@" \
+    -e "s@PIDFile=/var/run/.*@PIDFile=/var/run/${CMD}.pid@" ${CMD}.service.org > ${CMD}.service
